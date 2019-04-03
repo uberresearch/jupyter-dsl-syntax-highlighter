9640dc49afb4b80f8e082e98f3315d9291c63469
Carlo Panzi <carlo.panzi@gmail.com>
2019-04-03 10:53:52 +0100

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const extension = {
+  activate: () => {
+    console.log("ciao mamma");
+  },
+  autoStart: true,
+  id: "jupyterlab_code_formatter",
+  requires: []
+};
+
+export default extension;
